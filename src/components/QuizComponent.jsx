7da498bc8697d32331ec220d474f5ec68b9a0bee
ramkumar-1819 <rohitram1819@gmail.com>
2021-03-25 16:00:00 +0530

    Initial commit

---
+++ src/components/QuizComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Quiz.css'
+
+class Quiz extends React.Component{
+    render(){
+    return(<div className="contain">
+
+           <div className="question">
+                <div>Question</div>
+                <pre><span>  4 of 15</span>                                                                              Who is the first President of India?</pre>
+            </div>
+
+            <div className="answer">
+                <button>Dr.APJ.Abdul Kalam</button>
+                <button>Dr.Rajendra Prasad</button>
+                <button>Dr.Jawaharlal Nehru</button>
+                <button>Dr.Manmohan Singh</button>
+            </div>
+
+            <div className="buttons">
+                <button>Previous</button>
+                <button>Next</button>
+                <button>Quit</button>
+            </div>
+         </div>
+         )
+    }
+}
+export default Quiz
